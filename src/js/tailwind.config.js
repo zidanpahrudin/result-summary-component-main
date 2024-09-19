179dddf62e8e8c41a08d553850bbd34d61ca8c82
zidannyaman <zidan.pahrudin@nyaman.xyz>
2024-09-19 12:51:02 +0700

    first commit

---
+++ src/js/tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+          'sans': ['HankenGrotesk', 'sans-serif'],
+        },
+        colors: {
+            Lightred: "hsl(0, 100%, 67%)",
+            Orangeyyellow: "hsl(39, 100%, 56%)",
+            Greenteal: "hsl(166, 100%, 37%)",
+            Cobaltblue: "hsl(234, 85%, 45%)",
+            Lightslateblue: "hsl(252, 100%, 67%)",
+            Lightroyalblue: "hsl(241, 81%, 54%)",
+            Violetblue: "hsla(256, 72%, 46%, 1)",
+            Persianblue: "hsla(241, 72%, 46%, 0)",
+            White: "hsl(0, 0%, 100%)",
+            Paleblue: "hsl(221, 100%, 96%)",
+            Lightlavender: "hsl(241, 100%, 89%)",
+            Darkgrayblue: "hsl(224, 30%, 27%)"
+        }
+      }
+    }
+  }
